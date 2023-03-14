a1a1355651f17e2ab5ede75cfdcd683096ab97cc
muranuun <mura3-2033@hotmail.co.jp>
2023-03-14 13:26:45 +0900

    調整

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  useHead({
+    title: 'Home Page'
+  })
+</script>
+
+<template>
+  <div>
+    <!-- Welcome -->
+    <h1>Welcome to the Vue.js App</h1>
+    <!-- Navigation -->
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
+</template>

ebba94400a7110018c3dc96566e46e28aedc57fe
muranuun <mura3-2033@hotmail.co.jp>
2023-03-15 22:28:07 +0900

    created about page

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  useHead({
+    title: 'About'
+  })
+</script>
+
+<template>
+  <div>
+    <!-- Welcome -->
+    <h1 class="text-3xl">Welcome to the Vue.js App</h1>
+    <p>
+      This is a Vue.js app with TypeScript, Tailwind CSS, and Vite.
+    </p>
+  </div>
+</template>

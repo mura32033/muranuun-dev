ad3fa7ffc186ff844ea0d82596a507d4aee20ecb
muranuun <mura3-2033@hotmail.co.jp>
2023-03-23 17:41:04 +0900

    created navigation component

---
+++ components/Nav.vue
@@ -0,0 +1,6 @@
+<template>
+  <nav class="flex flex-row gap-4 mt-2 md:ml-auto md:mt-0 text-sm md:text-base">
+    <router-link to="/about">About</router-link>
+    <router-link to="/articles">Articles</router-link>
+  </nav>
+</template>

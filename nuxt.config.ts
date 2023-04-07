e1abd2a669234df9a9d9bac1e706faffc6d59c26
muranuun <mura3-2033@hotmail.co.jp>
2023-04-07 14:29:28 +0900

    removed contentful

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import 'dotenv/config'
 export default defineNuxtConfig({
   ssr: true,
   app: {
@@ -35,9 +34,5 @@ export default defineNuxtConfig({
       src: '~/plugins/vercel.ts',
       mode: 'client'
     }
-  ],
-  runtimeConfig: {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
-  }
+  ]
 })
--- pages/about.vue
+++ pages/about.vue
@@ -2,48 +2,10 @@
   useHead({
     title: 'About'
   })
-  // Get data via Contentful
-  const { data } = await useAsyncData('about', async (nuxtApp) => {
-    const { $contentfulClient } = nuxtApp
-    return await $contentfulClient.getEntries()
-  })
-  const item = data.value.items
-  let about = []
-  let award = []
-  let certification = []
-  let history = []
-  let presentation = []
-  let publication = []
-  let skill = []
-
-  item.forEach((i) => {
-    if(i.sys.contentType.sys.id === 'award') {
-      award.push(i.fields)
-    } else if(i.sys.contentType.sys.id === 'certification') {
-      certification.push(i.fields)
-    } else if(i.sys.contentType.sys.id === 'history') {
-      history.push(i.fields)
-    } else if(i.sys.contentType.sys.id === 'presentation') {
-      presentation.push(i.fields)
-    } else if(i.sys.contentType.sys.id === 'publication') {
-      publication.push(i.fields)
-    } else if(i.sys.contentType.sys.id === 'skill') {
-      skill.push(i.fields)
-    }
-  })
-  about.push({
-    'award': award,
-    'certification': certification,
-    'history': history,
-    'presentation': presentation,
-    'publication': publication,
-    'skill': skill
-  })
 </script>
 
 <template>
   <div>
     <Pagetitle title="About" />
-    <about-Award :content="about[0].award" />
   </div>
 </template>

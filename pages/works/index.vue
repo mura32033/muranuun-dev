7575938d3a1ff8d98e27763cb1d38ece0ad4713f
muranuun <mura3-2033@hotmail.co.jp>
2023-04-08 17:41:19 +0900

    fixed multiple root element

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-  useHead({
-    title: 'Works',
-    meta: [
-      {
-        name: 'description',
-        content: 'Works',
-      },
-    ],
-  })
+useHead({
+  title: 'Works',
+  meta: [
+    {
+      name: 'description',
+      content: 'Works',
+    },
+  ],
+})
 </script>
 <template>
-  <Pagetitle title="Works" />
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <ContentList path="/works">
-      <template v-slot="{ list }">
-        <WorkCard v-for="item in list" :key="item.id" :work="item" />
-      </template>
-      <template #not-found>
-        <p>No articles found.</p>
-      </template>
-    </ContentList>
+  <div>
+    <Pagetitle title="Works" />
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <ContentList path="/works">
+        <template v-slot="{ list }">
+          <WorkCard v-for="item in list" :key="item.id" :work="item" />
+        </template>
+        <template #not-found>
+          <p>No articles found.</p>
+        </template>
+      </ContentList>
+    </div>
   </div>
 </template>
